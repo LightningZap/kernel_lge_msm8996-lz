d8c222d55dea59b9ff6ddf72bf89c04ad4dc1f82
TeamFahQ <teamfahq1@gmail.com>
2017-07-08 12:49:31 -0500

    REBASE: Rebase using schwifty kernel. Import and configure build for H910 Rebased using swifty kernel and added defconfig for H910. Fixed Image.gz-drbb calls. Removed schwifty kernel commis to bring it back to stock (ish).

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export KBUILD_BUILD_USER=SaintZ93
 export KBUILD_BUILD_HOST=hidden
 export ARCH=arm64
-export CROSS_COMPILE=$HOME/obar/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-linaro-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/media/thomas/android/BrokenOs/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 DIR=$(pwd)
 BUILD="$DIR/build"
@@ -18,10 +18,10 @@ fi
 
 echo "setting up build..."
 mkdir "$BUILD"
-make O="$BUILD" elsa_spr_us-perf_defconfig
+make O="$BUILD" elsa_att_us-perf_defconfig
 
 echo "building kernel..."
-make O="$BUILD" -j$NPR
+make O="$BUILD" -j12
 
 echo "building modules..."
 make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
@@ -29,7 +29,7 @@ rm $BUILD/lib/modules/*/build
 rm $BUILD/lib/modules/*/source
 
 mkdir -p $OUT/modules
-mv "$BUILD/arch/arm64/boot/Image.lz4-dtb" "$OUT/Image.lz4-dtb"
+mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.lz4-dtb"
 find "$BUILD/lib/modules/" -name *.ko | xargs -n 1 -I '{}' mv {} "$OUT/modules"
 
 echo "Image.lz4-dtb and modules can be found in $OUT"
